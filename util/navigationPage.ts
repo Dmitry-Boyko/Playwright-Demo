0aef05082ba90f518979ae2016faaa75b101e9b2
Dmitry <dmt.b@yahoo.com>
2025-04-23 15:05:19 -0400

    added private method to verify if Form page expanded to avoid double click

--- util/navigationPage.ts
+++ util/navigationPage.ts
@@ -10,6 +10,19 @@ class NavigationPage {
     await this.page.getByText('Forms').click()
     await this.page.getByText('Form Layouts').click()
   }
+
+  async datePickerPage(){
+    await this.selectGroupMenuItem('Forms')
+    await this.page.getByText('Datepicker').click()
+  }
+
+  private async selectGroupMenuItem(groupitemTitle: string){
+    const groupMenuItem = this.page.getByTitle(groupitemTitle)
+    const expandedState = await groupMenuItem.getAttribute('area-expanded')
+    if(expandedState == 'false')
+      await groupMenuItem.click()
+  }
 }
+
 
 export{NavigationPage}
